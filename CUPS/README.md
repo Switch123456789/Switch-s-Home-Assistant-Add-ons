6743bf38551bb2d3a3eefd06e90f4f4c5d76aaef
Switch <25269380+Switch123456789@users.noreply.github.com>
2025-10-01 00:27:31 +0200

    overhaul

--- CUPS/README.md
+++ CUPS/README.md
@@ -1,26 +1,22 @@
 # Home Assistant Community Add-on: CUPS
 ### Features
 - Hosts a [CUPS](https://www.cups.org/) Server
-- Enables printer sharing to the local network.
+- Enables printer sharing to the local network
 - Announces printers on the local network over mDNS (zeroconf/avahi)
-- `cupsd.conf` is customizable through the CUPS-webinterface
-- `cupsd.conf` optionally can be restored to regain access (in case of misconfiguration)
+- Exposed CUPS & avahi config-files
 
 ### Restrictions
 - Can announce but not discover printers using mDNS
-- No preinstalled drivers (PPD-file needed)
+- Only stock and brother printers are preinstalled
 - No Ingress (yet?)
-
-
 # Help
 ### Installation
 1. Click: [![SHOW ADD-ON ON MY HOME ASSISTANT](https://my.home-assistant.io/badges/supervisor_addon.svg)](https://my.home-assistant.io/redirect/supervisor_addon/?addon=e34e2f70_cups&repository_url=https%3A%2F%2Fgithub.com%2FSwitch123456789%2FSwitch-s-Home-Assistant-Add-ons)
 2. Click OK on the pop-up to add my repo to Home Assistant
 3. Click install
 
-### Configuration
-- **Access the CUPS-webinterface** by navigating to `https://[HOSTNAME]:631` and edit the configurations as needed.
-- **Reset the configuration if necessary** by checking the appropriate box on the add-on configuration-page.
+### Infos
+- **Webinterface:** Can be accessed by navigating to `https://[HOSTNAME]:631`
+- **Config-files:** Can be accessed through `addon_configs`-folder
 
-### More infos
-- [Repo add-on-page](https://github.com/Switch123456789/Switch-s-Home-Assistant-Add-ons)
+- **More infos:** Are on [the repo add-on-page](https://github.com/Switch123456789/Switch-s-Home-Assistant-Add-ons)
--- CUPS/run.sh
+++ CUPS/run.sh
@@ -1,31 +1,22 @@
 #!/usr/bin/with-contenv bashio
-ulimit -n 1048576 #fix
-
 # PREPARATIONS ##################################################################
-    bashio::log.info "PREPARING CUPS ..."
-        if [ ! -d "/data/cups" ]; then
-            bashio::log.notice "NO CONFIGURATION FOUND, USING DEFAULTS!"
-            cp -p -r "/etc/cups.default" "/data/cups"
-        fi
-        ln -s "/data/cups" "/etc"
-#################################################################################
-
-
+    bashio::log.info "CHECKING CONFIGURATION-FILES ..."
+    if [ ! -d "/config/cups" ]; then
+        bashio::log.notice "NO CUPS CONFIGURATION FOUND, USING DEFAULTS!"
+        cp -p -r "/etc/cups.default" "/config/cups"
+    fi
+    ln -s "/config/cups" "/etc"
+    if [ ! -d "/config/avahi" ]; then
+        bashio::log.notice "NO AVAHI CONFIGURATION FOUND, USING DEFAULTS!"
+        cp -p -r "/etc/avahi.default" "/config/avahi"
+    fi
+    ln -s "/config/avahi" "/etc"
+    cupsd -t
 # START CUPS ####################################################################
-    #RESET CONFIGURATION
-        if [ "$(bashio::config 'on_startup')" = "Reset configuration" ]; then bashio::log.info "RESETTING CONFIGURATION ..."
-            cp -p "/etc/cups.default/cupsd.conf" "/data/cups"
-    #RESET CUPS
-        elif [ "$(bashio::config 'on_startup')" = "Reset CUPS" ]; then bashio::log.info "RESETTING CUPS ..."
-            rm -r "/etc/cups/"* && cp -p -r "/etc/cups.default/"* "/data/cups"
-    #START CUPS
-        else bashio::log.info "STARTING SERVICES ..."
-            dbus-daemon --system
-            avahi-daemon -D
-            sleep 5
-            bashio::log.info "CHECKING CONFIGURATION-FILES ..."
-            cupsd -t
-            bashio::log.info "STARTING CUPS ..."
-            cupsd -f
-        fi
-            bashio::log.info "ADDON STOPPED!"
+    bashio::log.info "STARTING SERVICES ..."
+    dbus-daemon --system
+    avahi-daemon -D
+    sleep 5
+    bashio::log.info "STARTING CUPS ..."
+    cupsd -f
+    bashio::log.info "ADDON STOPPED!"
